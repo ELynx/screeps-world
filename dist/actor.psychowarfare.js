3ec294e4b3d06511529adfb0dc0875f271e52b65
ELynx <1621251+ELynx@users.noreply.github.com>
2019-02-19 13:49:23 +0300

    improvement - module for psychological warfare

---
+++ dist/actor.psychowarfare.js
@@ -0,0 +1,96 @@
+/**
+Psycho of the day text.
+Ten characters long. Infinitely scary and/or hilarious.
+**/
+
+// When there is only one creep in a room, imitate dialog
+const potdSingle = [
+//  |----------| limit
+    'Wait!',
+   ,'Dont attac'
+   ,'Exploring'
+   ,'Will leave'
+   ,'Sorry'
+   ,'roomsimnot'
+   ,'room[sim]n'
+   ,'DAMN'
+   ,'OK got it.'
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,'DAMN!!!!!'
+   ,''
+   ,''
+   ,''
+   ,'suiside'
+   ,'suiside'
+   ,'suicide'
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+   ,''
+];
+
+// TODO
+// When there are more creeps, just spam
+// Each creep gets a line
+const potdGroup = [
+//  |----------| limit
+    'E2-E4'
+   ,'pew pew'
+   ,'im hit'
+   ,'4 da swarm'
+   ,'heal me'
+   ,'ERROR'
+   ,'Spawn1'
+   ,'creep.say('
+   ,'all your b'
+   ,'ase are be'
+   ,'long to us'
+   ,'il be back'
+   ,'fus-ro-DAH'
+];
+
+var psychoWarfareActor =
+{
+    indexCache = { };
+
+    /**
+    @param {Room} room
+    **/
+    act: function(room)
+    {
+        const creeps = room.find(FIND_MY_CREEPS);
+
+        if (creeps.length == 0)
+            return;
+
+        this.indexCache[room.id] = this.indexCache[room.id] || 0;
+
+        for (var i = 0; i < creeps.length; ++i)
+        {
+            var index = this.indexCache[room.id];
+
+
+            creeps[i].say(potdSingle[this.indexCache[room.id]++]);
+
+            if (this.indexCache[room.id] >= potdSingle.length)
+            {
+                this.indexCache[room.id] = 0;
+            }
+        }
+    }
+};
+
+module.exports = psychoWarfareActor;
