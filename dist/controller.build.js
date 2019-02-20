55c2a3c8981d69a0823f651ded8e488651bf93d7
ELynx <1621251+ELynx@users.noreply.github.com>
2019-02-20 18:18:53 +0300

    basic repair

--- dist/controller.build.js
+++ dist/controller.build.js
@@ -4,14 +4,43 @@ var buildController = new Controller('build');
 
 buildController.actDistance = 3;
 
-buildController.act = function(site, creep)
+buildController.act = function(target, creep)
 {
-    return creep.build(site) == OK;
+    if (target instanceof ConstructionSite)
+    {
+        return creep.build(target) == OK;
+    }
+    else
+    {
+        // TODO decide
+        creep.repair(target);
+        return false; // not sticky
+    }
 };
 
 buildController.targets = function(room)
 {
-    return room.find(FIND_MY_CONSTRUCTION_SITES);
+    const sites = room.find(FIND_MY_CONSTRUCTION_SITES);
+
+    const structs = room.find(FIND_MY_STRUCTURES,
+        {
+            filter: function(structure)
+            {
+                if (structure instanceof StructureWall)
+                {
+                    // STRATEGY TODO depend on level
+                    return structure.hits < 1000;
+                }
+                else
+                {
+                    // STRATEGY TODO don't run with every booboo
+                    return structure.hits <  Math.ceil(structure.maxHits * 0.85);
+                }
+            }
+        }
+    );
+
+    return sites.concat(structs);
 };
 
 module.exports = buildController;
