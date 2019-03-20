ec9a490d66d21a6a988a0ac8a348dca4eff0e304
ELynx <1621251+ELynx@users.noreply.github.com>
2019-03-20 12:36:20 +0300

    pest neighbor

--- dist/main.js
+++ dist/main.js
@@ -60,6 +60,76 @@ module.exports.loop = function ()
             }
         }
         // >>
+
+        // temporary code, steal energy from neighbour
+        // <<
+        var pest = Game.creeps['pest'];
+        if (pest)
+        {
+            if (pest.carry.energy < pest.carryCapacity)
+            {
+                if (pest.pos.roomName == 'E38N1')
+                {
+                    pest.moveTo(49, 19, 'E38N1');
+                }
+                else
+                {
+                    const wallPos = new RoomPosition(2, 19, 'E39N1');
+                    const stuff = wallPos.look();
+                    if (stuff.length > 0)
+                    {
+                        if (pest.pos.isNearTo(stuff[0]))
+                        {
+                            pest.dismantle(stuff[0]);
+                        }
+                        else
+                        {
+                            pest.move(RIGHT);
+                        }
+                    }
+                    else
+                    {
+                        const spawn = Game.getObjectById('5c8d3f57360c03411ed406f8');
+                        if (pest.pos.isNearTo(spawn))
+                        {
+                            pest.withdraw(spawn, RESOURCE_ENERGY);
+                        }
+                        else
+                        {
+                            pest.moveTo(spawn, { maxRooms: 1, range: 1 });
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (pest.fatigue == 0)
+                {
+                    if (pest.pos.roomName == 'E39N1')
+                    {
+                        pest.moveTo(0, 19, 'E39N1');
+                    }
+                    else
+                    {
+                        const dropPoint = new RoomPosition(34, 8, 'E38N1');
+
+                        if (pest.pos.isEqualTo(dropPoint))
+                        {
+                            pest.drop(RESOURCE_ENERGY);
+                        }
+                        else
+                        {
+                            pest.moveTo(dropPoint. { maxRooms: 1, plainCost: 2 });
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            Game.getObjectById('5c8f93046ce2ec3bb9d19a9e').spawnCreep([CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE], 'pest');
+        }
+        // >>
     }
 
     //console.log('Used ' + Game.cpu.getUsed());
