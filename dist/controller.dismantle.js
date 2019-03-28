eaade3aecc0a7ddbd715079adc19cc282d8d5814
ELynx <1621251+ELynx@users.noreply.github.com>
2019-03-28 08:47:36 +0300

    some basic logic

--- dist/controller.dismantle.js
+++ dist/controller.dismantle.js
@@ -21,6 +21,83 @@ dismantleController.act = function(structure, creep)
 
 dismantleController.dynamicTargets = function(room, creep)
 {
+    // TODO unify
+    for (const flagName in Game.flags)
+    {
+        if (!flagName.startsWith('dism'))
+        {
+            continue;
+        }
+
+        const flag = Game.flags[flagName];
+
+        if (flag.pos.roomName != room.name)
+        {
+            continue;
+        }
+
+        var types = undefined;
+
+        if (flag.secondaryColor == COLOR_GREY)
+        {
+            types = [STRUCTURE_WALL, STRUCTURE_RAMPART];
+        }
+        else if (flag.secondaryColor == COLOR_GREEN)
+        {
+            types = [STRUCTURE_RAMPART];
+        }
+        else if (flag.secondaryColor == COLOR_WHITE)
+        {
+            types = [STRUCTURE_ROAD];
+        }
+
+        if (!types)
+        {
+            continue;
+        }
+
+        var range = 3; // red, for brevity
+
+        if (flag.color == COLOR_PURPLE)
+        {
+            range = 5;
+        }
+        else if (flag.color == COLOR_YELLOW)
+        {
+            range = 2;
+        }
+        else if (flag.color == COLOR_GREEN)
+        {
+            range = 1;
+        }
+        else if (flag.color == COLOR_BLUE)
+        {
+            range = 0;
+        }
+
+        const dynamic = this._lookAroundCreep(
+            room,
+            LOOK_STRUCTURES,
+            function(structure)
+            {
+                if (types.indexOf(structure.structureType) != -1)
+                {
+                    return true;
+                }
+            },
+            creep
+        );
+
+        if (dynamic.length == 0)
+        {
+            flag.remove();
+        }
+        else
+        {
+            return dynamic;
+        }
+    }
+
     return [];
 };
 
