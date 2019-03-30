038d57bbd12d8d79948fa269dc47d037497505b3
ELynx <1621251+ELynx@users.noreply.github.com>
2019-03-30 15:41:04 +0300

    controller.controller - don't limit # of upgrades

--- dist/controller.controller.js
+++ dist/controller.controller.js
@@ -6,22 +6,9 @@ var controllerController = new Controller('controller');
 
 controllerController.actRange = 3;
 
-controllerController.extra = function(controller)
-{
-    // STRATEGY maximum number of upgradeController calls per assignment
-    return 10;
-};
-
 controllerController.act = function(controller, creep)
 {
-    const rc = creep.upgradeController(controller);
-
-    if (rc == OK)
-    {
-        return --creep.memory.xtra >= 0;
-    }
-
-    return false; 
+    return creep.upgradeController(controller) == OK;
 };
 
 controllerController.staticTargets = function(room)
