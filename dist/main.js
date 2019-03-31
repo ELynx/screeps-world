c0e3af6c536080fed6ac4eb38c857c147580453c
ELynx <1621251+ELynx@users.noreply.github.com>
2019-03-31 18:25:33 +0300

    money generator

--- dist/main.js
+++ dist/main.js
@@ -33,5 +33,72 @@ module.exports.loop = function()
     manualStrelok();
     manualClaim();
 
+    // free market
+    // <<
+    if (Memory.order && Memory.stopPrice)
+    {
+        var stopIt = true;
+
+        const orderInfo = Game.market.getOrderById(Memory.order);
+        if (orderInfo)
+        {
+            if (orderInfo.price < Memory.stopPrice)
+            {
+                const buy = Math.random() < 0.16;
+
+                if (buy)
+                {
+                    stopIt = false;
+
+                    Game.market.deal(Memory.order, 1000, 'E38N1');
+                }
+                else
+                {
+                    const allOrders = Game.market.getAllOrders(
+                        function(order)
+                        {
+                            return order.type == ORDER_BUY &&
+                                   order.resourceType == orderInfo.resourceType &&
+                                   order.price > 2 * orderInfo.price &&
+                                   Game.map.getRoomLinearDistance(order.roomName, 'E39N1', true) < 30;
+                        }
+                    );
+
+                    if (allOrders.length > 0)
+                    {
+                        allOrders.sort(
+                            function(o1, o2)
+                            {
+                                if (o1.price == o2.price)
+                                {
+                                    return Game.map.getRoomLinearDistance(o1.roomName, 'E39N1', true) - Game.map.getRoomLinearDistance(o2.roomName, 'E39N1', true);
+                                }
+
+                                return o2.price - o1.price;
+                            }
+                        );
+
+                        for (var i = 0; i < allOrders.length; ++i)
+                        {
+                            if (Game.market.deal(allOrders[i].id, 500, 'E38N1') == OK)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    stopIt = false;
+                }
+            }
+        }
+
+        if (stopIt)
+        {
+            delete Memory.order;
+            delete Memory.stopPrice;
+        }
+    }
+    // >>
+
     //console.log('Used       ' + Game.cpu.getUsed());
 }
