da66084f1736dc9f74981c5af384c97a869fae67
Eduard <1621251+ELynx@users.noreply.github.com>
2019-04-03 05:56:21 +0300

    Update manual.strelok.js

--- dist/manual.strelok.js
+++ dist/manual.strelok.js
@@ -27,6 +27,23 @@ var strelok = function()
         ++now;
         roomCount[dest] = now;
 
+        if (creep.pos.x == 0)
+        {
+            creep.move(RIGHT);
+        }
+        else if (creep.pos.y == 0)
+        {
+            creep.move(BOTTOM);
+        }
+        else if (creep.pos.x == 49)
+        {
+            creep.move(LEFT);
+        }
+        else if (creep.pos.y == 49)
+        {
+            creep.move(TOP);
+        }
+        
         if (dest != creep.pos.roomName)
         {
             if (creep.fatigue == 0)
@@ -48,23 +65,6 @@ var strelok = function()
         }
         else
         {
-            if (creep.pos.x == 0)
-            {
-                creep.move(RIGHT);
-            }
-            else if (creep.pos.y == 0)
-            {
-                creep.move(BOTTOM);
-            }
-            else if (creep.pos.x == 49)
-            {
-                creep.move(LEFT);
-            }
-            else if (creep.pos.y == 49)
-            {
-                creep.move(TOP);
-            }
-            
             if (!roomTargets[dest])
             {
                 // first wipe spawn
