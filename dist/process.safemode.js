a3e4fbad63780c20c7456da37607ef3395e9279a
ELynx <1621251+ELynx@users.noreply.github.com>
2019-04-10 18:36:34 +0300

    process.safemode - monitor threat level

--- dist/process.safemode.js
+++ dist/process.safemode.js
@@ -4,57 +4,91 @@ var Process = require('process.template');
 
 var safemodeProcess = new Process('safemode');
 
+const ThreatStep = 20;
+const ThreatMax  = 5;
+
 safemodeProcess.work = function(room, hostileCreeps)
 {
     this.debugHeader(room);
 
+    let threatLevel = room.memory.threat  || 0;
+    let threatTimer = room.memory.threatT || (Game.time - ThreatStep);
+
     if (hostileCreeps.length > 0)
     {
-        const ctrl = room.controller;
-
-        if (ctrl && ctrl.my)
+        if (threatTimer + ThreatStep < Game.time)
         {
-            if (!ctrl.safeMode &&
-                !ctrl.safeModeCooldown &&
-                !ctrl.upgradeBlocked &&
-                 ctrl.safeModeAvailable > 0)
+            if (threatLevel < ThreatMax)
             {
-                for (const flagName in Game.flags)
+                ++threatLevel;
+                console.log(room.name + ' threat escalated to ' + threatLevel);
+            }
+
+            threatTimer = Game.time;
+        }
+
+        const ctrl = room.controller;
+        if (!ctrl.safeMode &&
+            !ctrl.safeModeCooldown &&
+            !ctrl.upgradeBlocked &&
+             ctrl.safeModeAvailable > 0)
+        {
+            for (const flagName in Game.flags)
+            {
+                if (!flagName.startsWith('fence'))
                 {
-                    if (!flagName.startsWith('fence'))
-                    {
-                        continue;
-                    }
+                    continue;
+                }
 
-                    const flag = Game.flags[flagName];
+                const flag = Game.flags[flagName];
 
-                    if (flag.pos.roomName != room.name)
-                    {
-                        continue;
-                    }
+                if (flag.pos.roomName != room.name)
+                {
+                    continue;
+                }
 
-                    let range = flag.getValue();
+                let range = flag.getValue();
 
-                    if (range < 0)
-                    {
-                        continue;
-                    }
+                if (range < 0)
+                {
+                    continue;
+                }
 
-                    const trigger = flag.pos.findInRange(hostileCreeps, range);
-                    if (trigger.length > 0)
-                    {
-                        const rc = ctrl.activateSafeMode();
+                const trigger = flag.pos.findInRange(hostileCreeps, range);
+                if (trigger.length > 0)
+                {
+                    const rc = ctrl.activateSafeMode();
 
-                        const notification = 'Room ' + room.name + ' requested safeMode [' + rc + ']';
+                    const notification = 'Room ' + room.name + ' requested safeMode [' + rc + ']';
 
-                        Game.notify(notification);
-                        console.log(notification);
+                    Game.notify(notification);
+                    console.log(notification);
 
-                        break;
-                    }
+                    break;
                 }
             }
         }
+    }
+    else
+    {
+        if (threatTimer + ThreatStep < Game.time)
+        {
+            --threatLevel;
+            threatTimer = Game.time;
+
+            console.log(room.name + ' threat deescalated to ' + threatLevel);
+        }
+    }
+
+    if (threatLevel > 0)
+    {
+        room.memory.threat = threatLevel;
+        room.memory.threatT = threatTimer;
+    }
+    else
+    {
+        room.memory.threat = undefined;
+        room.memory.threatT = undefined;
     }
 };
 
