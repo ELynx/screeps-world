a7f2d1168d1560019522e49e72a2cb78df76b5b6
ELynx <1621251+ELynx@users.noreply.github.com>
2019-04-12 17:30:44 +0300

    survival of the fittest renewal code

--- dist/controller.ttl.js
+++ dist/controller.ttl.js
@@ -5,8 +5,8 @@ var Controller = require('controller.template');
 
 var ttlController = new Controller('ttl');
 
-// STRATEGY time when creep is called for renew
-const Ttl = 100;
+// STRATEGY time when stronger creep is called for renew
+const Ttl = 200;
 
 ttlController.actRange = 1;
 
@@ -18,66 +18,39 @@ ttlController.enough = function(room)
 
 ttlController.act = function(spawn, creep)
 {
+    // by default creep is here only for renewal
+    let renew   = true;
     let recycle = false;
 
-    if (!recycle)
+    // if spawning started
+    if (spawn.spawning)
     {
-        // example parameters
-        const [level, exampleBody] = bodywork[creep.memory.btyp](spawn.room.memory.elvl);
-
-        if (creep.memory.levl < level)
-        {
-            recycle = true;
-        }
-        else if (creep.memory.levl == level)
-        {
-            if (exampleBody.length == creep.body.length)
-            {
-                for (let i = 0; i < exampleBody.length && !recycle; ++i)
-                {
-                    recycle = exampleBody[i] != creep.body[i].type;
-                }
-            }
-            else
-            {
-                recycle = true;
-            }
-        }
-    }
-
-    let renew = !recycle;
-    let rc = false;
-
-    if (renew && spawn.spawning)
-    {
+        // if creep will not live long enough
         if (creep.ticksToLive <= spawn.spawning.remainingTime)
         {
+            // just give some resources back
+            renew   = false;
             recycle = true;
         }
         else
         {
-            renew = false;
-            rc = true;
+            // wait
+            renew   = false;
+            recycle = false;
         }
-    }
-
-    // donate while here
-    if (spawn.energy < spawn.energyCapacity && creep.carry.energy > 0)
-    {
-        creep.transfer(spawn, RESOURCE_ENERGY);
     }
 
     if (recycle)
     {
-        rc = spawn.recycleCreep(creep) == OK;
+        return spawn.recycleCreep(creep) == OK;
     }
 
     if (renew)
     {
-        rc = spawn.renewCreep(creep) == OK;
+        return spawn.renewCreep(creep) == OK;
     }
 
-    return rc;
+    return true;
 };
 
 ttlController.staticTargets = function(room)
@@ -99,8 +72,34 @@ ttlController.filterCreep = function(creep)
         return false;
     }
 
+    // just fast check to skip repeated state check
+    if (creep.memory.xttl)
+    {
+        return false;
+    }
+
     // STRATEGY don't drag resources around
-    return creep.sumCarry() > 0;
+    if (creep.sumCarry() > 0)
+    {
+        return false;
+    }
+
+    // do background check
+    const room = Game.rooms[creep.memory.crum];
+    if (room)
+    {
+        // renew creeps that are higher level than room can produce
+        const [level, exampleBody] = bodywork[creep.memory.btyp](room.memory.elvl);
+        if (creep.memory.levl > level)
+        {
+            return true;
+        }
+    }
+
+    // flag to avoid repeated checks
+    creep.memory.xttl = true;
+
+    return false;
 };
 
 ttlController.register();
