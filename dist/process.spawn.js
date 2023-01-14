a4256a48c3e5e3eec0e9689b5bc9f696436d89cc
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-15 00:00:36 +0100

    Create process.spawn.js

---
+++ dist/process.spawn.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Process = require('process.template');
+var globals = require('globals');
+var queue   = require('routine.spawn');
+
+var spawnProcess = new Process('spawn');
+
+spawnProcess.work = function(room)
+{
+    this.debugHeader(room);
+
+    const haveOrSpawning = room.getRoomControlledCreeps().length;
+    const inQueue = queue.count(room.name);
+
+    const want = 6;
+
+    const delta = want - haveOrSpawning - inQueue;
+    if (delta > 0)
+    {
+        queue.addLowkey(
+            room.name,
+            'worker',
+            'worker_' + room.name,
+            {
+                crum: room.name,
+                ctrl: globals.NO_CONTROL,
+                dest: globals.NO_DESTINATION,
+                dact: globals.NO_ACT_RANGE,
+                xtra: globals.NO_EXTRA,
+                btyp: undefined,
+                levl: undefined,
+                hvst: true,
+                rstk: undefined,
+                minr: undefined
+            },
+            room.name,
+            room.name,
+            delta
+        );
+    }
+};
+
+spawnProcess.register();
+
+module.exports = spawnProcess;
