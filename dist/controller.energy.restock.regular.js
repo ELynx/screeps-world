667503e6ac6691b5f6f859b403a7a060488c1553
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-17 00:32:35 +0100

    Update controller.energy.restock.regular.js

--- dist/controller.energy.restock.regular.js
+++ dist/controller.energy.restock.regular.js
@@ -5,8 +5,9 @@ var Controller = require('controller.template');
 var energyRestockControllerRegular = new Controller('energy.restock.regular');
 
 // STRATEGY parameters for restocking
-const TowerRestockMult = 0.9;
-const TerminalRestock  = 300;
+const TowerRestockNormal   = 0.9;
+const TowerRestockCritical = 0.25;
+const TerminalRestock      = 300;
 
 energyRestockControllerRegular.actRange = 1;
 
@@ -29,33 +30,46 @@ energyRestockControllerRegular.act = function(target, creep)
 
 energyRestockControllerRegular.staticTargets = function(room)
 {
+    const critical = room.find(
+        FIND_STRUCTURES,
+        {
+            filter: function(structure)
+            {
+                if (structure.my && structure.isActiveSimple())
+                {
+                    if (structure.structureType == STRUCTURE_TOWER)
+                    {
+                        return structure.store.getUsedCapacity(RESOURCE_ENERGY) < TowerRestockCritical * structure.store.getCapacity(RESOURCE_ENERGY);
+                    }
+                }
+
+                return false;              
+            }
+        }
+    );
+
+    if (critical.length > 0) return critical;
+
     return room.find(
         FIND_STRUCTURES,
         {
             filter: function(structure)
             {
-                if (!structure.my)
+                if (structure.my && structure.isActiveSimple())
                 {
-                    return false;
-                }
-
-                if (!structure.isActiveSimple())
-                {
-                    return false;
-                }
-
-                if (structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_EXTENSION)
-                {
-                    return structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-                else if (structure.structureType == STRUCTURE_TOWER)
-                {
-                    return structure.store.getUsedCapacity(RESOURCE_ENERGY) < TowerRestockMult * structure.store.getCapacity(RESOURCE_ENERGY);
-                }
-                else if (structure.structureType == STRUCTURE_TERMINAL)
-                {
-                    return structure.store.getUsedCapacity(RESOURCE_ENERGY) < TerminalRestock;
+                    if (structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_EXTENSION)
+                    {
+                        return structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                    else if (structure.structureType == STRUCTURE_TOWER)
+                    {
+                        return structure.store.getUsedCapacity(RESOURCE_ENERGY) < TowerRestockNormal * structure.store.getCapacity(RESOURCE_ENERGY);
+                    }
+                    else if (structure.structureType == STRUCTURE_TERMINAL)
+                    {
+                        return structure.store.getUsedCapacity(RESOURCE_ENERGY) < TerminalRestock;
+                    }
                 }
 
                 return false;              
