52e63f7353887b93ac2bef2fa4ef64105c233ad1
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-22 20:27:24 +0100

    WIP

---
+++ dist/controller.grab.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var globals    = require('globals');
+var Controller = require('controller.template');
+
+var grabController = new Controller('grab');
+
+grabController.actRange = 1;
+
+grabController.act = function(currentController, creep)
+{
+    const hasUniversalStore = creep.room.storage || creep.room.terminal;
+
+    const [t, l, b, r] = creep.pos.squareArea(1);
+    const grabs = creep.room.lookAtArea(t, l, b, r, true);
+
+    for (let i = 0; i < grabbs.length; ++i)
+    {
+        const grab = grabs[i];
+        const from = grab[grabb.type];
+
+        if (grab.type == LOOK_TOMBSTONES ||
+            grab.type == LOOK_RUINS)
+        {
+            const typesToGrab = hasUniversalStore ? Object.keys(from.store) : [ RESOURCE_ENERGY ];
+
+            for (let j = 0; j < typesToGrab.length; ++j)
+            {
+                const typeToGrab = typesToGrab[j];
+                if (from.store.getUsedCapacity(typeToGrab) > 0)
+                {
+                    const rc = this.wrapIntent(creep, 'withdraw', from, typeToGrab);
+                    if (rc == globals.WARN_LAST_INTENT) return rc;
+                }
+            }
+        }
+
+        if (grabb.type == LOOK_RESOURCES)
+        {
+            if (hasUniversalStore || from.resourceType == RESOURCE_ENERGY)
+            {
+                const rc = this.wrapIntent(creep, 'pickup', from);
+                if (rc == globals.WARN_LAST_INTENT) return rc;
+            }
+        }
+    }
+
+    return OK;
+};
+
+grabController.filterCreep = function(creep)
+{
+    return creep.store.getFreeCapacity() > 0;
+};
+
+// NOT registered, called from room actor explicitly
+
+module.exports = grabController;
