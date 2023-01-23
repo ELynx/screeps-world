5437d57e8bd70231cf6460cb05f59e62520c6753
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-23 11:44:16 +0100

    wip

--- dist/controller.template.js
+++ dist/controller.template.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var globals        = require('globals');
+var intentSolver   = require('routine.intent');
 var makeDebuggable = require('routine.debuggable');
 
 const profiler = require('screeps-profiler');
@@ -479,30 +480,7 @@ function Controller(id)
 
     this.wrapIntent = function(creep, intentName, arg0 = undefined, arg1 = undefined, arg2 = undefined)
     {
-        // hotfix
-        if (intentName == 'harvest')
-        {
-            if (creep.store.getFreeCapacity() == 0) return ERR_FULL;
-            if (creep.store.getFreeCapacity() <= creep.getActiveBodyparts(WORK) * HARVEST_POWER) return globals.WARN_LAST_INTENT;
-        }
-
-        const intent = creep[intentName];
-        if (intent === undefined)
-        {
-            console.log('Invalid intent [' + intentName + '] called for creep [' + creep.name + ']');
-            return globals.ERR_INVALID_INTENT_NAME;
-        }
-
-        const boundIntent = _.bind(intent, creep);
-
-        let rc = globals.ERR_INVALID_INTENT_ARG;
-
-        if      (arg2 !== undefined) rc = boundIntent(arg0, arg1, arg2);
-        else if (arg1 !== undefined) rc = boundIntent(arg0, arg1);
-        else if (arg0 !== undefined) rc = boundIntent(arg0);
-        else                         rc = boundIntent();
-
-        return rc;
+        return intentSolver.wrapCreepIntent(creep, intentName, arg0, arg1, arg2);
     };
 
     /**
