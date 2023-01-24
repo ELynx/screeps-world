d3500533e3bbd4bc43e081cd741d3c306a2f4d6b
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-25 00:27:33 +0100

    more functional

---
+++ dist/tasked.observe.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Tasked = require('tasked.template');
+
+var observe = new Tasked('observe');
+
+observe.creepAtDestination = function(creep)
+{
+    const pos = creep.getControlPos();
+    if (creep.pos.x != pos.x || creep.pos.y != pos.y)
+    {
+        creep.moveToWrapper(pos, { maxRooms: 1 });
+    }
+};
+
+observe.flagPrepare = function(flag)
+{
+    if (flag.room) return this.FLAG_IGNORE;
+    return this.FLAG_SPAWN;
+};
+
+observe.makeBody = function(spawn)
+{
+    return [MOVE];
+};
+
+observe.register();
+
+module.exports = observe;
