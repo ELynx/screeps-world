494944362d80659195f233379d82f575cb0a9a7d
Eduard <1621251+ELynx@users.noreply.github.com>
2023-01-26 19:14:17 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,23 @@
-module.exports = function(grunt)
-{
-    const env_vars = process.env;
+module.exports = function (grunt) {
+  const env_vars = process.env
 
-    grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-screeps')
 
-    grunt.initConfig(
-      {
-        screeps:
+  grunt.initConfig(
+    {
+      screeps:
         {
-            options:
+          options:
             {
-                email:  env_vars.PUSH_EMAIL,
-                token:  env_vars.PUSH_TOKEN,
-                branch: env_vars.PUSH_BRANCH
+              email: env_vars.PUSH_EMAIL,
+              token: env_vars.PUSH_TOKEN,
+              branch: env_vars.PUSH_BRANCH
             },
-            dist:
+          dist:
             {
-                src: ['dist/*.js']
+              src: ['dist/*.js']
             }
         }
-      }
-    );
+    }
+  )
 }
