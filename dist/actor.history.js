145afdbb0ba5d40f9c27156b0e26a6610b9fdae8
Eduard <1621251+ELynx@users.noreply.github.com>
2023-02-04 14:29:18 +0100

    Update actor.history.js

--- dist/actor.history.js
+++ dist/actor.history.js
@@ -121,10 +121,10 @@ const historyActor =
       // Neutral (24) to Hostile (-1) in 9 actions
       // Neutral (0) to Hostile (-1) in 1 action
       const reputation = Game.iff.decreaseReputation(attackerUsername, 3)
-      this.debugLine(room, this.hmiName(attacker) + ' owned by ' + attackerUsername + ' had owner reputation changed to ' + reputation)
+      this.debugLine(room, this.hmiName(attacker) + ' owned by PC [' + attackerUsername + '] had owner reputation changed to ' + reputation)
     } else {
       const reputation = Game.iff.markNPCHostile(attacker)
-      this.debugLine(room, this.hmiName(attacker) + ' NPC had reputation changed to ' + reputation)
+      this.debugLine(room, this.hmiName(attacker) + ' owned by NPC [' + attackerUsername + '] had reputation changed to ' + reputation)
     }
 
     // SHORTCUT check hostile once
