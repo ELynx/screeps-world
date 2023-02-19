a125f84ba9865e0ae7515ab9a735055da2fd78f4
Eduard <1621251+ELynx@users.noreply.github.com>
2023-02-19 17:34:25 +0100

    remove not urgent functions

--- dist/routine.intent.js
+++ dist/routine.intent.js
@@ -387,51 +387,10 @@ const intent = {
   },
 
   spawn_intent_renewCreep: function (spawn, creep) {
-    if (creep === undefined || creep.body === undefined) {
-      console.log('spawn_intent_renewCreep received invalid argument [creep]')
-      return bootstrap.ERR_INVALID_INTENT_ARG
-    }
-
-    if (_.some(creep.body, _.matchesProperty('type', CLAIM))) {
-      console.log('spawn_intent_renewCreep received creep with CLAIM part, cannot renew')
-      return ERR_INVALID_TARGET
-    }
-
-    const spawningKey = '__spawning'
-    const spawningValue = spawn.spawning
-    const spawning = this.getIntended(spawn, spawningKey, spawningValue)
-    if (spawning) {
-      return ERR_BUSY
-    }
-
-    const ttlPower = Math.floor(SPAWN_RENEW_RATIO * CREEP_LIFE_TIME / CREEP_SPAWN_TIME / creep.body.length);
-    const ttlKey = '__ticksToLive'
-    const ttlValue = creep.ticksToLive
-    const ttl = this.getWithIntended(creep, ttlKey, ttlValue)
-    if (ttl + ttlPower > CREEP_LIFE_TIME) {
-      return ERR_FULL
-    }
-
-    const bodyCost = this._creepBodyCost(creep.body)
-    const energyCost = Math.ceil(SPAWN_RENEW_RATIO * bodyCost / CREEP_SPAWN_TIME / creep.body.length);
-    const roomEnergyKey = '__energyAvailable'
-    const roomEnergyValue = spawn.room.energyAvailable
-    const roomEnergy = this.getWithIntended(spawn.room, roomEnergyKey, roomEnergyValue)
-    if (roomEnergy < energyCost) {
-      return ERR_NOT_ENOUGH_ENERGY
-    }
-
-
-
     return OK
   },
 
   spawn_intent_recycleCreep: function (spawn, creep) {
-    if (creep === undefined || creep.body === undefined) {
-      console.log('spawn_intent_recycleCreep received undefined argument [creep]')
-      return bootstrap.ERR_INVALID_INTENT_ARG
-    }
-
     return OK
   },
 
