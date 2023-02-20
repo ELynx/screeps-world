ad45059643fb9acf22a402ae969ddce74dda74b6
Eduard <1621251+ELynx@users.noreply.github.com>
2023-02-20 23:21:02 +0100

    Update tasked.economist.js

--- dist/tasked.economist.js
+++ dist/tasked.economist.js
@@ -7,15 +7,21 @@ const Tasked = require('./tasked.template')
 const economist = new Tasked('economist')
 
 economist.act = function () {
+  if (globals.economist === undefined) {
+    globals.economist = { }
+  }
+
+  const ringIndex = Game.time % ENERGY_REGEN_TIME
+
   for (const roomName in Game.rooms) {
     const room = Game.rooms[roomName]
 
-    const intents = intentSolver.getRoomIntents(room)
+    if (room.my) {
+      const intents = intentSolver.getRoomIntents(room)
 
-    const energySpent = intents.__spent_total || 0
-    const energyAcquired = intents.__acquired_total || 0
-
-    console.log(roomName + ' spent ' + energySpent + ' and acquired ' + energyAcquired)
+      const energySpent = intents.__spent_total || 0
+      const energyAcquired = intents.__acquired_total || 0
+    }
   }
 }
 
