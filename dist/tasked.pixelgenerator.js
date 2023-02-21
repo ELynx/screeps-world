a53105344cde4e77b015f93ea98ee89ff78c18de
Eduard <1621251+ELynx@users.noreply.github.com>
2023-02-21 14:56:04 +0100

    lint

--- dist/tasked.pixelgenerator.js
+++ dist/tasked.pixelgenerator.js
@@ -5,7 +5,7 @@ const Tasked = require('./tasked.template')
 const pixelGenerator = new Tasked('pixelGenerator')
 
 pixelGenerator.act = function () {
-  if (Game.cpu.bucket === 10000) {
+  if (Game.cpu.bucket >= PIXEL_CPU_COST) {
     Game.cpu.generatePixel()
   }
 }
