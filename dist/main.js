3fabf3a9c7ee60e918651a4d5dc3da251975b4ba
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-02 11:50:53 +0100

    shuffle a bit

--- dist/main.js
+++ dist/main.js
@@ -39,36 +39,21 @@ module.exports.loop = function () {
 
     historyActor.act()
 
-    const limits = { }
-    let total = 0
+    // prevent division by zero
+    const total = Math.max(1, _.keys(Game.creeps).length)
 
-    for (const creepName in Game.creeps) {
-      const creep = Game.creeps[creepName]
-      const room = creep.room
-
-      // STRATEGY limit weight for controlled vs other rooms, per creep
-      const delta = room.my ? 3 : 2
-
-      let now = limits[room.name] || 0
-      now = now + delta
-      limits[room.name] = now
-
-      total = total + delta
-    }
-
-    if (total === 0) total = 1
-
-    for (const roomName in Game.rooms) {
+    const roomNames = _.shuffle(_.keys(Game.rooms))
+    for (const roomName of roomNames) {
       const room = Game.rooms[roomName]
 
-      const r = limits[room.name] || 0
+      // STRATEGY CPU limit allocation for controlled vs other rooms
+      const r = _.keys(room.creeps).length * (room.my ? 3 : 2)
       const t = total
       const limit = Math.ceil(100 * r / t)
 
       room.visual.rect(5.5, 0, 5, 0.5, { fill: '#0f0' })
       room.visual.rect(5.5, 0, 5 * r / t, 0.25, { fill: '#03f' })
 
-      // save CPU on all rooms where control is not needed
       if (room.my) {
         room.memory.cpul = limit
         roomActor.act(room)
