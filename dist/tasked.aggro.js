e2b1e7d6557f6e2f745739cdd74a7bfea28f8308
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-03 14:52:32 +0100

    mnjhjhg

--- dist/tasked.aggro.js
+++ dist/tasked.aggro.js
@@ -5,30 +5,22 @@ const Tasked = require('./tasked.template')
 const aggro = new Tasked('aggro')
 
 aggro.act = function () {
-  for (const roomName in Game.rooms) {
-    const room = Game.rooms[roomName]
-    room.__aggro = []
-    room.__breached = undefined
-  }
-
   const flags = Game.flagsByShortcut[this.id] || []
-  for (const index in flags) {
-    const flag = flags[index]
+  for (const flag of flags) {
     const room = flag.room
 
     if (room) {
       const atPos = flag.pos.lookFor(LOOK_STRUCTURES)
 
-      const aggros = _.filter(
+      const atPosTargets = _.filter(
         atPos,
         function (structure) {
           return structure.hits && structure.structureType !== STRUCTURE_ROAD
         }
       )
 
-      room.__aggro = room.__aggro.concat(aggros)
-
-      room.__breached = room.__aggro.length === 0
+      room.addAggro(atPosTargets)
+      room.setBreached(room.aggro().length === 0)
     }
   }
 }
