fe5d2095f191ad0a6b1ba26f4c297c4ee26b4d90
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-03 14:33:26 +0100

    Update routine.cleanup.js

--- dist/routine.cleanup.js
+++ dist/routine.cleanup.js
@@ -2,11 +2,15 @@
 
 const bootstrap = require('./bootstrap')
 
+// STRATEGY give unaccessed memory node N ticks before deletion
+
+const RoomNodeMaxAge = 60 * CREEP_LIFE_TIME // rouglhy 3 days
+const StructureNodeMaxAge = 3 * CREEP_LIFE_TIME
+
 const cleanup = {
-  memoryNodeNotAccessed: function (memoryNode) {
+  memoryNodeNotAccessed: function (memoryNode, age) {
     if (memoryNode.nodeAccessed) {
-      // STRATEGY give unaccessed memory node N ticks before deletion
-      return memoryNode.nodeAccessed < (Game.time - 5)
+      return memoryNode.nodeAccessed < (Game.time - age)
     }
 
     return true
@@ -15,15 +19,13 @@ const cleanup = {
   cleanupMemory: function () {
     for (const name in Memory.creeps) {
       if (!Game.creeps[name]) {
-        if (this.memoryNodeNotAccessed(Memory.creeps[name])) {
-          delete Memory.creeps[name]
-        }
+        delete Memory.creeps[name]
       }
     }
 
     for (const name in Memory.rooms) {
       if (!Game.rooms[name]) {
-        if (this.memoryNodeNotAccessed(Memory.rooms[name])) {
+        if (this.memoryNodeNotAccessed(Memory.rooms[name], RoomNodeMaxAge)) {
           delete Memory.rooms[name]
         }
       }
@@ -31,51 +33,44 @@ const cleanup = {
 
     for (const name in Memory.flags) {
       if (!Game.flags[name]) {
-        if (this.memoryNodeNotAccessed(Memory.flags[name])) {
-          delete Memory.flags[name]
-        }
+        delete Memory.flags[name]
       }
     }
 
     if (Memory.structures) {
       for (const id in Memory.structures) {
         if (!Game.structures[id]) {
-          if (this.memoryNodeNotAccessed(Memory.structures[id])) {
+          if (this.memoryNodeNotAccessed(Memory.structures[id], StructureNodeMaxAge)) {
             delete Memory.structures[id]
           }
         }
       }
     }
-
-    if (Memory.reputation) {
-      if (Object.keys(Memory.reputation).length === 0) {
-        delete Memory.reputation
-      }
-    }
   },
 
   cleanupFlags: function () {
-    const taskIds = Object.keys(bootstrap.taskControllers)
-    const processIds = Object.keys(bootstrap.processControllers)
+    const taskIds = _.keys(bootstrap.taskControllers)
+    const processIds = _.keys(bootstrap.processControllers)
     const flagKeys = _.map(taskIds.concat(processIds), id => id + '_')
 
     for (const flagName in Game.flags) {
       if (flagName === 'profiler') continue
       if (flagName === 'recount') continue
       if (flagName === 'autobuild') continue
+      if (flagName === 'dashboard') continue
 
-      const processFound = _.some(
+      const controllerFound = _.some(
         flagKeys,
         function (id) {
           return flagName.startsWith(id)
         }
       )
 
-      if (!processFound) {
-        const flag = Game.flags[flagName]
-        console.log('Removing undefined flag [' + flagName + '] at ' + flag.pos)
-        flag.remove()
-      }
+      if (controllerFound) continue
+
+      const flag = Game.flags[flagName]
+      console.log('Removing undefined flag [' + flagName + '] at ' + flag.pos)
+      flag.remove()
     }
   },
 
