57e31420bb25e6354d2fb91a5f48b8569f06c0f1
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-10 22:51:24 +0100

    Update actor.room.js

--- dist/actor.room.js
+++ dist/actor.room.js
@@ -316,7 +316,7 @@ const roomActor =
               creep.__target,
               {
                 costCallback: mapUtils.costCallback_costMatrixForRoomActivity,
-                ignoreCreeps: true,
+                ignoreCreeps: false,
                 maxRooms: 1,
                 range: creep.memory.dact,
                 reusePath: _.random(3, 5)
