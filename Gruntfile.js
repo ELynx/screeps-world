37a3798ef5fa77c26ce634a6e8e26e09f17ea28f
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-12 17:00:34 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,30 @@
 module.exports = function (grunt) {
   const envVars = process.env
 
+  const options =
+  {
+    email: envVars.PUSH_EMAIL,
+    token: envVars.PUSH_TOKEN,
+    branch: envVars.PUSH_BRANCH
+  }
+
+  if (envVars.PUSH_PTR) {
+    options.ptr = true
+  }
+
+  const dist =
+  {
+    src: [envVars.PUSH_WHAT]
+  }
+
   grunt.loadNpmTasks('grunt-screeps')
 
   grunt.initConfig(
     {
       screeps:
         {
-          options:
-            {
-              email: envVars.PUSH_EMAIL,
-              token: envVars.PUSH_TOKEN,
-              branch: envVars.PUSH_BRANCH,
-              ptr: envVars.PUSH_PTR
-            },
-          dist:
-            {
-              src: [envVars.PUSH_WHAT]
-            }
+          options,
+          dist
         }
     }
   )
