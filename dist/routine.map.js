7606035be7b5ed10410cccfb3c621b16efdcca2f
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-12 19:26:59 +0100

    Update routine.map.js

--- dist/routine.map.js
+++ dist/routine.map.js
@@ -4,23 +4,6 @@ if (!(new PathFinder.CostMatrix())._bits) {
   console.log('CostMatrix missing _bits')
 }
 
-PathFinder.CostMatrix.prototype.setBordersUnwalkable = function () {
-  if (!this._bits) {
-    return
-  }
-
-  for (let index = 0; index <= 49; ++index) {
-    this._bits[index] = 255
-    this._bits[index + 2450] = 255
-  }
-
-  for (let row = 1; row <= 48; ++row) {
-    const index0 = row * 50
-    this._bits[index0] = 255
-    this._bits[index0 + 49] = 255
-  }
-}
-
 PathFinder.CostMatrix.prototype.setStationaryCreepsUnwalkabke = function (roomName) {
   if (!this._bits) {
     return
@@ -37,24 +20,6 @@ PathFinder.CostMatrix.prototype.setStationaryCreepsUnwalkabke = function (roomNa
 }
 
 const map = {
-  costCallback_costMatrixWithUnwalkableBorders: function (roomName, costMatrix) {
-    if (Game.__unwalkableBordersCostCallbackCache === undefined) {
-      Game.__unwalkableBordersCostCallbackCache = { }
-    }
-
-    const cached = Game.__unwalkableBordersCostCallbackCache[roomName]
-    if (cached) {
-      return cached
-    }
-
-    const modified = costMatrix.clone()
-    modified.setBordersUnwalkable()
-
-    Game.__unwalkableBordersCostCallbackCache[roomName] = modified
-
-    return modified
-  },
-
   costCallback_costMatrixForRoomActivity: function (roomName, costMatrix) {
     if (Game.__roomActivityCostCallbackCache === undefined) {
       Game.__roomActivityCostCallbackCache = { }
@@ -65,13 +30,11 @@ const map = {
       return cached
     }
 
-    const modified = costMatrix.clone()
-    modified.setBordersUnwalkable()
-    modified.setStationaryCreepsUnwalkabke(roomName)
+    costMatrix.setStationaryCreepsUnwalkabke(roomName)
 
-    Game.__roomActivityCostCallbackCache[roomName] = modified
+    Game.__roomActivityCostCallbackCache[roomName] = costMatrix
 
-    return modified
+    return costMatrix
   }
 }
 
