c1aa6422d2926df51c8c556af256f210c40ec1cf
Eduard <1621251+ELynx@users.noreply.github.com>
2023-03-18 01:20:44 +0100

    Update demand.and.supply.js

--- dist/demand.and.supply.js
+++ dist/demand.and.supply.js
@@ -4,16 +4,20 @@ const intentSolver = require('./routine.intent')
 
 const noDemand =
 {
-  priority: 99
+  priority: 90
 }
 
 const noSupply =
 {
-  priority: 99
+  priority: 90
 }
 
 const simpleDemand = function (something, type, priority) {
   const amount = intentSolver.getFreeCapacity(something, type)
+
+  if (amount <= 0) {
+    return noDemand
+  }
 
   const demand =
   {
@@ -27,10 +31,31 @@ const simpleDemand = function (something, type, priority) {
 const simpleSupply = function (something, type, priority) {
   const amount = intentSolver.getUsedCapacity(something, type)
 
+  if (amount <= 0) {
+    return noSupply
+  }
+
   const supply =
   {
     priority,
     [type]: amount
+  }
+
+  return supply
+}
+
+const universalStoreSupply = function (something, priority) {
+  const supply =
+  {
+    priority
+  }
+
+  const resourceTypes = _.keys(something.store)
+  for (const resourceType of resourceTypes) {
+    const amount = intentSolver.getUsedCapacity(something, resourceType)
+    if (amount > 0) {
+      supply[resourceType] = amount
+    }
   }
 
   return supply
@@ -57,11 +82,27 @@ Object.defineProperty(
 )
 
 Object.defineProperty(
-  StructureSpawn.prototype,
-  'demand',
+  StructureContainer.prototype,
+  'demand_restocker',
   {
     get: function () {
-      return simpleDemand(this, RESOURCE_ENERGY, 50)
+      if (this.isSource()) {
+        return simpleDemand(this, RESOURCE_ENERGY, 0)
+      } else {
+        return noDemand
+      }
+    },
+    configurable: true,
+    enumerable: true
+  }
+)
+
+Object.defineProperty(
+  StructureContainer.prototype,
+  'supply',
+  {
+    get: function () {
+      return universalStoreSupply(this, 65)
     },
     configurable: true,
     enumerable: true
@@ -70,6 +111,50 @@ Object.defineProperty(
 
 Object.defineProperty(
   StructureExtension.prototype,
+  'demand',
+  {
+    get: function () {
+      return simpleDemand(this, RESOURCE_ENERGY, 51)
+    },
+    configurable: true,
+    enumerable: true
+  }
+)
+
+Object.defineProperty(
+  StructureLink.prototype,
+  'demand_restocker',
+  {
+    get: function () {
+      if (this.isSource()) {
+        return simpleDemand(this, RESOURCE_ENERGY, 0)
+      } else {
+        return noDemand
+      }
+    },
+    configurable: true,
+    enumerable: true
+  }
+)
+
+Object.defineProperty(
+  StructureLink.prototype,
+  'supply',
+  {
+    get: function () {
+      if (this.isSource()) {
+        return noSupply
+      } else {
+        return simpleSupply(this, RESOURCE_ENERGY, 64)
+      }
+    },
+    configurable: true,
+    enumerable: true
+  }
+)
+
+Object.defineProperty(
+  StructureSpawn.prototype,
   'demand',
   {
     get: function () {
