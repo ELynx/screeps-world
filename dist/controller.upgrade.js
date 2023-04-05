3a75ca98e40a5297a61a9fb8448d75370524670d
Eduard <1621251+ELynx@users.noreply.github.com>
2023-04-06 01:45:36 +0200

    Update controller.upgrade.js

--- dist/controller.upgrade.js
+++ dist/controller.upgrade.js
@@ -10,6 +10,22 @@ upgradeController.act = function (controller, creep) {
   return this.wrapIntent(creep, 'upgradeController', controller)
 }
 
+upgradeController.validateTarget = function (allTargets, target, creep) {
+  // no imit below level 8
+  if (target.level < 8) {
+    return true
+  }
+
+  let othersWork = 0
+
+  const others = this._allAssignedTo(target)
+  for (const other of others) {
+    othersWork += _.countBy(other.body, 'type')[WORK] || 0
+  }
+
+  return othersWork < CONTROLLER_MAX_UPGRADE_PER_TICK
+}
+
 upgradeController.targets = function (room) {
   // don't upgrade controller in room with active fight
   if (room.__fight) {
