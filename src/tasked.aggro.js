bacefdccb6856f3982b0aff2be23d8d1daff351f
Eduard <1621251+ELynx@users.noreply.github.com>
2023-04-13 00:25:05 +0200

    Revert "scope"
    This reverts commit 1161e84d6d7632da00e53a2edc25c610da4a4f1b.

--- src/tasked.aggro.js
+++ src/tasked.aggro.js
@@ -6,20 +6,18 @@ const aggro = new Tasked('aggro')
 
 aggro.act = function () {
   const flags = Game.flagsByShortcut[this.id] || []
-
-  if (flags.length === 0) return
-
-  const aggroOn = function (structure) {
-    return structure.hits && structure.structureType !== STRUCTURE_ROAD
-  }
-
   for (const flag of flags) {
     const room = flag.room
 
     if (room) {
       const atPos = flag.pos.lookFor(LOOK_STRUCTURES)
 
-      const atPosTargets = _.filter(atPos, aggroOn)
+      const atPosTargets = _.filter(
+        atPos,
+        function (structure) {
+          return structure.hits && structure.structureType !== STRUCTURE_ROAD
+        }
+      )
 
       if (flag.name.indexOf('!') !== -1) {
         for (const atPosTarget of atPosTargets) {
