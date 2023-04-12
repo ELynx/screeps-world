a4fbeef0aed973a58c4f3b7828891e02cd0b95ef
Eduard <1621251+ELynx@users.noreply.github.com>
2023-04-13 00:25:50 +0200

    Revert "remove unused"
    This reverts commit 6044774d8e3a1d5024ef92dc48d8f7ba5ff92f72.

--- src/controller.upgrade.js
+++ src/controller.upgrade.js
@@ -10,7 +10,7 @@ upgradeController.act = function (controller, creep) {
   return this.wrapIntent(creep, 'upgradeController', controller)
 }
 
-upgradeController.validateTarget = function (target) {
+upgradeController.validateTarget = function (allTargets, target, creep) {
   // no limit below level 8
   if (target.level < 8) {
     return true
