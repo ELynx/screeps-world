c9980a21686113172bcd1e8f23abfba174e83cb3
ELynx <ELynx@users.noreply.github.com>
2023-11-13 16:58:43 +0000

    [MegaLinter] Apply linters fixes

--- dist/bootstrap.js
+++ dist/bootstrap.js
@@ -25,14 +25,14 @@ const bootstrap = {
   ERR_INTENDED_EXHAUSTED: -10003,
 
   adjacentDirections: {
-    [TOP]:          [TOP_LEFT,     TOP_RIGHT],
-    [TOP_RIGHT]:    [TOP,          RIGHT],
-    [RIGHT]:        [TOP_RIGHT,    BOTTOM_RIGHT],
-    [BOTTOM_RIGHT]: [RIGHT,        BOTTOM],
-    [BOTTOM]:       [BOTTOM_RIGHT, BOTTOM_LEFT],
-    [BOTTOM_LEFT]:  [BOTTOM,       LEFT],
-    [LEFT]:         [BOTTOM_LEFT,  TOP_LEFT],
-    [TOP_LEFT]:     [LEFT,         TOP]
+    [TOP]: [TOP_LEFT, TOP_RIGHT],
+    [TOP_RIGHT]: [TOP, RIGHT],
+    [RIGHT]: [TOP_RIGHT, BOTTOM_RIGHT],
+    [BOTTOM_RIGHT]: [RIGHT, BOTTOM],
+    [BOTTOM]: [BOTTOM_RIGHT, BOTTOM_LEFT],
+    [BOTTOM_LEFT]: [BOTTOM, LEFT],
+    [LEFT]: [BOTTOM_LEFT, TOP_LEFT],
+    [TOP_LEFT]: [LEFT, TOP]
   },
 
   /**
