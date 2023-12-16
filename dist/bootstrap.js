08b8d8565c613c1970430c285af2a823bbc29c31
ELynx <1621251+ELynx@users.noreply.github.com>
2023-12-16 01:42:30 +0100

    track move

--- dist/bootstrap.js
+++ dist/bootstrap.js
@@ -35,6 +35,17 @@ const bootstrap = {
     [TOP_LEFT]: [LEFT, TOP]
   },
 
+  directionToDelta: {
+    [TOP]: [0, -1],
+    [TOP_RIGHT]: [1, -1],
+    [RIGHT]: [1, 0],
+    [BOTTOM_RIGHT]: [1, 1],
+    [BOTTOM]: [0, 1],
+    [BOTTOM_LEFT]: [-1, 1],
+    [LEFT]: [-1, 0],
+    [TOP_LEFT]: [-1, -1]
+  },
+
   /**
    Same as built-in, but tries to get some shortcuts
    **/
