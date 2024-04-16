4953e07a13fc018a742d5302954676044e1fb2ae
ELynx <1621251+ELynx@users.noreply.github.com>
2024-04-16 22:18:14 +0200

    simplify

--- dist/controller.upgrade.js
+++ dist/controller.upgrade.js
@@ -12,43 +12,27 @@ upgradeController.act = function (controller, creep) {
   return this.wrapIntent(creep, 'upgradeController', controller)
 }
 
-upgradeController._validateUpgradeLimit = function (target) {
-  // no limit below level 8
-  if (target.level < 8) {
-    return true
-  }
-
-  let othersWork = 0
-  const others = this._allAssignedTo(target)
-  for (const other of others) {
-    bootstrap.activeBodyParts(other)
-    othersWork += other._work_
-  }
-
-  return othersWork < CONTROLLER_MAX_UPGRADE_PER_TICK
-}
-
-upgradeController.validateTarget = function (allTargets, target, creep) {
-  if (!this._validateTarget(allTargets, target, creep)) return false
-
-  return this._validateUpgradeLimit(target)
-}
-
-upgradeController.targets = function (room) {
+upgradeController._roomQualified = function (room) {
   // don't upgrade controller in room with active fight
   if (room._fight_) {
-    return []
+    return false
   }
 
   if (room.controller.upgradeBlocked) {
+    return false
+  }
+
+  return true
+}
+
+upgradeController.targets = function (room) {
+  if (!this._roomQualified(room)) {
     return []
   }
 
-  // keep non specialists away from upgrades
-  if (this.id !== upgradeControllerSpecialist.id) {
-    if (room.getRoomControlledUpgraders().length > 0) {
-      return []
-    }
+  // leave 8 and above :) to specialists
+  if (room.controller.level >= 8) {
+    return []
   }
 
   return [room.controller]
@@ -59,7 +43,15 @@ const upgradeControllerSpecialist = _.assign({ }, upgradeController)
 
 upgradeControllerSpecialist.id = 'upgrade.specialist'
 
-upgradeController.validateTarget = undefined // just go for it, block all others as well
+upgradeControllerSpecialist.validateTarget = undefined // TODO actually bypass range check only, not all checks
+
+upgradeControllerSpecialist.targets = function (room) {
+  if (!this._roomQualified(room)) {
+    return []
+  }
+
+  return [room.controller]
+}
 
 upgradeControllerSpecialist.filterCreep = function (creep) {
   return this._defaultFilter(creep) && this._isUpgrader(creep)
