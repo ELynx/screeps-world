3c967ae13216e1ede31fd7cd28f296193c0c1a2a
ELynx <1621251+ELynx@users.noreply.github.com>
2024-04-21 19:50:57 +0200

    delegate

--- dist/actor.cook.js
+++ dist/actor.cook.js
@@ -2,6 +2,11 @@
 
 const cookActor =
 {
+  roomCanHandleNonEnergy: function (room) {
+    console.log('TODO canRoomGrabNonEnergy => true')
+    return room !== undefined
+  },
+
   act: function () {
     console.log('We need to cook')
   }
