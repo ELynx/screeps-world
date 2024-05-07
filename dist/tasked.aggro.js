b9c6981cf969d073ebd401f697e1ede676bf3a8e
ELynx <1621251+ELynx@users.noreply.github.com>
2024-05-08 00:01:47 +0200

    lintttt

--- dist/tasked.aggro.js
+++ dist/tasked.aggro.js
@@ -5,19 +5,17 @@ const Tasked = require('./tasked.template')
 const aggro = new Tasked('aggro')
 
 aggro.act = function () {
+  const isAggroTarget = structure => {
+    return structure.hits && structure.structureType !== STRUCTURE_ROAD
+  }
+
   const flags = Game.flagsByShortcut[this.id] || []
   for (const flag of flags) {
     const room = flag.room
 
     if (room) {
       const atPos = flag.pos.lookFor(LOOK_STRUCTURES)
-
-      const atPosTargets = _.filter(
-        atPos,
-        structure => {
-          return structure.hits && structure.structureType !== STRUCTURE_ROAD // false positive skipcq: JS-0073
-        }
-      )
+      const atPosTargets = _.filter(atPos, isAggroTarget)
 
       if (flag.name.indexOf('!') !== -1) {
         for (const atPosTarget of atPosTargets) {
