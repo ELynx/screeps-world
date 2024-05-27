680051562625006e53a79ca7ef3ffc2a6a62a026
ELynx <1621251+ELynx@users.noreply.github.com>
2024-05-27 22:18:56 +0200

    Update tasked.nuker.js

--- dist/tasked.nuker.js
+++ dist/tasked.nuker.js
@@ -16,23 +16,23 @@ nuker.act = function () {
   const nukers = Array.from(Game.nukers.values())
   if (nukers.length === 0) return
 
+  const sortByDistance = (n1, n2) => {
+    const d1 = Game.map.getRoomLinearDistance(n1.room.name, flag.pos.roomName)
+    const d2 = Game.map.getRoomLinearDistance(n2.room.name, flag.pos.roomName)
+
+    return d1 - d2
+  }
+
   for (const flag of flags) {
     const local = nukers.slice(0)
-    local.sort(
-      (n1, n2) => {
-        const d1 = Game.map.getRoomLinearDistance(n1.room.name, flag.pos.roomName)
-        const d2 = Game.map.getRoomLinearDistance(n2.room.name, flag.pos.roomName)
+    local.sort(sortByDistance)
 
-        return d1 - d2
-      }
-    )
+    for (const someNuker of local) {
+      if (someNuker._launched_) continue
 
-    for (const nuker of local) {
-      if (nuker._launched_) continue
-
-      const rc = nuker.launchNuke(flag.pos)
+      const rc = someNuker.launchNuke(flag.pos)
       if (rc >= OK) {
-        nuker._launched_ = true
+        someNuker._launched_ = true
         flag.remove()
         break // from local cycle
       }
