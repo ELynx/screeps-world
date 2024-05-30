e9298f6d3274c9dc7523cf395f787d3b62815497
ELynx <1621251+ELynx@users.noreply.github.com>
2024-05-30 16:39:39 +0200

    Update routine.map.js

--- dist/routine.map.js
+++ dist/routine.map.js
@@ -1,40 +1,41 @@
 'use strict'
 
+let MapRoutineDisabled = false
+
 if (!(new PathFinder.CostMatrix())._bits) {
   console.log('CostMatrix missing _bits')
+  MapRoutineDisabled = true
 }
 
-PathFinder.CostMatrix.prototype.setStationaryCreepsUnwalkabke = function (roomName) {
+PathFinder.CostMatrix.prototype.blockArray = function (blocks) {
   if (!this._bits) {
     return
   }
 
-  const room = Game.rooms[roomName]
-  if (room === undefined) return
-
-  for (const block of room.blocked) {
+  for (const block of blocks) {
     // https://github.com/screeps/engine/blob/78d980e50821ea9956d940408b733c44fc9d94ed/src/game/path-finder.js#L25
     this._bits[block.x * 50 + block.y] = 255
     room.visual.circle(block.x, block.y, { fill: 'transparent', radius: 0.55, stroke: 'white' })
   }
-
-  // reset array for re-entry
-  room.blocked = []
 }
 
 const map = {
   costCallback_costMatrixForRoomActivity: function (roomName, costMatrix) {
-    if (Game.__map__costCallback_costMatrixForRoomActivity_cache === undefined) {
-      Game.__map__costCallback_costMatrixForRoomActivity_cache = new Map()
+    if (MapRoutineDisabled) return undefined
+
+    const room = Game.rooms[roomName]
+    if (room === undefined) return undefined
+
+    if (room.blocked.length > 0) {
+      const costMatrix1 = room.__map__cache1 || costMatrix
+
+      costMatrix1.blockArray(room.blocked)
+      room.blocked = []
+
+      room.__map__cache1 = costMatrix1
     }
 
-    const currentState = Game.__map__costCallback_costMatrixForRoomActivity_cache.get(roomName) || costMatrix
-
-    currentState.setStationaryCreepsUnwalkabke(roomName)
-
-    Game.__map__costCallback_costMatrixForRoomActivity_cache.set(roomName, currentState)
-
-    return currentState
+    return room.__map__cache1 // not a problem if undefined
   }
 }
 
