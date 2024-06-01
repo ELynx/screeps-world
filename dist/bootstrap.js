5e4e43ea7f42a05a3a9a206d44032bec922b595d
ELynx <1621251+ELynx@users.noreply.github.com>
2024-06-01 21:36:52 +0200

    some corrections

--- dist/bootstrap.js
+++ dist/bootstrap.js
@@ -297,20 +297,20 @@ const bootstrap = {
   },
 
   _isHighwayRoomName (parsed) {
-    return parsed.length === 2 && (parsed[0] % 10 === 0 || parsed[1] % 10 === 0)
+    return parsed.length === 3 && (parsed[1] % 10 === 0 || parsed[2] % 10 === 0)
   },
 
   _isSourceKeeperRoomName (parsed) {
-    if (parsed.length !== 2) return false
-    const x = parsed[0] % 10
+    if (parsed.length !== 3) return false
+    const x = parsed[1] % 10
     if (x < 4 || x > 6) return false
-    const y = parsed[1] % 10
+    const y = parsed[2] % 10
     if (y < 4 || y > 6) return false
     return !(x === 5 && y === 5)
   },
 
   _isSectorCenterRoomName (parsed) {
-    return parsed.length === 2 && (parsed[0] % 10 === 5 && parsed[1] % 10 === 5)
+    return parsed.length === 3 && (parsed[1] % 10 === 5 && parsed[2] % 10 === 5)
   },
 
   isHighwayRoomName (roomName) {
