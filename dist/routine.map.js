0ac2c92a66e2c8091038dd63f27b576cc53ca534
ELynx <1621251+ELynx@users.noreply.github.com>
2024-06-01 23:58:08 +0200

    eh

--- dist/routine.map.js
+++ dist/routine.map.js
@@ -168,7 +168,10 @@ const map = {
       if (dest.x === destinationPosition.x && dest.y === destinationPosition.y && dest.room === destinationPosition.roomName) {
         const time = creep.memory._march.time
         if (time >= (Memory.roomOwnerChangeDetected || 0)) {
-          if (creep.memory._march.room !== creep.room.name) {
+          // takes care of stepping into the room and running out of CPU at the same time
+          if (creep.memory._march.room !== creep.room.name &&
+              creep.pos.x !== 0 && creep.pos.x !== 49 &&
+              creep.pos.y !== 0 && creep.pos.y !== 49) {
             creep.memory._march.path = creep.memory._march.path.slice(1)
             creep.memory._march.room = creep.room.name
           }
