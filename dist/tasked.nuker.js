ea9df79f819b56733f05446b16da68628dfa654b
ELynx <1621251+ELynx@users.noreply.github.com>
2024-06-07 21:23:44 +0200

    duct tape

--- dist/tasked.nuker.js
+++ dist/tasked.nuker.js
@@ -5,6 +5,20 @@ const Tasked = require('./tasked.template')
 const nuker = new Tasked('nuker')
 
 nuker.act = function () {
+  // << this is duct tape but I am losing my cycle of special interest to it will stay here for a while
+  for (const roomName of Game.roomsToScan.values()) {
+    for (const observer of Game.observers.values()) {
+      if (observer._observed_) continue
+
+      const rc = observer.observeRoom(roomName)
+      if (rc >= OK) {
+        observer._observed_ = true
+        break // from observer loop
+      }
+    }
+  }
+  // >>
+
   const flags = Game.flagsByShortcut[this.id] || []
   if (flags.length === 0) return
 
